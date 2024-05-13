5f01bb97d7a7c3d3e873cb8e87840827a2a9ba80
CodeYouness <ylijassi@gmail.com>
2024-05-13 16:12:21 +0200

    added if to check if the number is > or < than the computer number

--- Gioco dei dadi/js/script.js
+++ Gioco dei dadi/js/script.js
@@ -1,4 +1,12 @@
 const computerNumber = Math.floor(Math.random() * 6) + 1;
 const userNumber = Math.floor(Math.random() * 6) + 1;
 
-console.log(userNumber, computerNumber)
+if (computerNumber < userNumber) {
+    console.log('complimenti! hai vinto! il tuo numero fortunato è stato ' + userNumber + ' quello del computer era: ' + computerNumber)
+} else if (computerNumber > userNumber) {
+    console.log('complimenti! hai perso! il tuo numero sfortunato è stato ' + userNumber + ' quello del computer era: ' + computerNumber)
+} else {
+    console.log('azz, qui ce un pareggio! vi è capitato ad entrambi il ' + userNumber)
+}
+
+console.log(computerNumber, userNumber)
