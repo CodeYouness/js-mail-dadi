9cb3209fa99f62a003af9f1c7b2fd1bd4b536ef0
CodeYouness <ylijassi@gmail.com>
2024-05-13 16:18:39 +0200

    added const for maillist and usermail

---
+++ Mail/js/script.js
@@ -0,0 +1,6 @@
+const mailList = ['capomafia@hotmail.com', 'lamiamail@mail.com', 'mailsegreta@protonmail.com']
+const userMail = prompt('hey yo, whats your email?')
+
+if (mailList.includes(userMail)) {
+    console.log()
+}
