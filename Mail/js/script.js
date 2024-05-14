ce92109287a2dee5b7ffd167056f160370e52319
CodeYouness <ylijassi@gmail.com>
2024-05-14 10:01:41 +0200

    added .tolowercase and .trim to fix font for check email

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,7 +1,7 @@
 const mailList = ['capomafia@hotmail.com', 'lamiamail@mail.com', 'mailsegreta@protonmail.com']
 const userMail = prompt('hey yo, whats your email?')
 
-if (mailList.includes(userMail)) {
+if (mailList.includes(userMail.toLowerCase().trim())) {
     console.log('congratulations! you re in')
 } else {
     console.log('godo, you re not in')
